ebaeb696d32df68b631a4b5ab4dcb6d28a0a8d29
Rory Brosnan <rbrosnan@gmail.com>
2016-07-25 22:19:34 -0400

    feat: directives

---
+++ src/app/recipe-book.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes';
+import { ShoppingListComponent } from './shopping-list';
+
+@Component({
+  moduleId: module.id,
+  selector: 'recipe-book-app',
+  templateUrl: 'recipe-book.component.html',
+  directives: [HeaderComponent, RecipesComponent, ShoppingListComponent]
+})
+export class RecipeBookAppComponent {
+}
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe';
+import { RecipeItemComponent } from './recipe-item.component';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-recipe-list',
+  templateUrl: 'recipe-list.component.html',
+  directives: [RecipeItemComponent]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'https://www.swirtshaus.de/sites/default/files/schnitzel-swirtshaus-friedrichshafen-bodensee-m_0.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+}
