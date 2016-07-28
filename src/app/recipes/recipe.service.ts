793e4d11bc444b6d4f89c98e7633b35274fbde45
Rory Brosnan <rbrosnan@gmail.com>
2016-07-27 21:02:50 -0400

    feat: http
    completed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
 
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
+
   private recipes: Recipe[] = [
       new Recipe('Schnitzel', 'Very tasty', 'https://www.swirtshaus.de/sites/default/files/schnitzel-swirtshaus-friedrichshafen-bodensee-m_0.jpg', [
         new Ingredient('French Fries', 2),
@@ -13,7 +16,7 @@ export class RecipeService {
       new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
     ];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -34,4 +37,24 @@ export class RecipeService {
   editRecipe (oldRecipe: Recipe, newRecipe: Recipe){
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.put('https://recipebook-dda4e.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+
+  fetchData() {
+    return this.http.get('https://recipebook-dda4e.firebaseio.com/recipes.json')
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data: Recipe[]) => {
+          this.recipes = data;
+          this.recipesChanged.emit(this.recipes);
+        }
+      );
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 import { RecipeBookAppComponent, environment } from './app/';
 import { ShoppingListService } from './app/shopping-list';
@@ -11,4 +12,10 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(RecipeBookAppComponent, [APP_ROUTES_PROVIDERS, ShoppingListService, disableDeprecatedForms(), provideForms()]);
+bootstrap(RecipeBookAppComponent, [
+  APP_ROUTES_PROVIDERS,
+  ShoppingListService,
+  provideForms(),
+  disableDeprecatedForms(),
+  HTTP_PROVIDERS
+]);
