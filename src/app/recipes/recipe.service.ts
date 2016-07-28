d7900bd4fe6c90555d5426bab196081cfbfebc28
Rory Brosnan <rbrosnan@gmail.com>
2016-07-27 20:29:28 -0400

    feat: forms
    completed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,7 +7,7 @@ import { Ingredient } from '../shared';
 export class RecipeService {
   private recipes: Recipe[] = [
       new Recipe('Schnitzel', 'Very tasty', 'https://www.swirtshaus.de/sites/default/files/schnitzel-swirtshaus-friedrichshafen-bodensee-m_0.jpg', [
-        new Ingredient('French Fires', 2),
+        new Ingredient('French Fries', 2),
         new Ingredient('Pork Meat', 1)
       ]),
       new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
@@ -25,5 +25,13 @@ export class RecipeService {
 
   deleteRecipe(recipe: Recipe) {
     this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  addRecipe (recipe: Recipe) {
+    this.recipes.push();
+  }
+
+  editRecipe (oldRecipe: Recipe, newRecipe: Recipe){
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
 }
